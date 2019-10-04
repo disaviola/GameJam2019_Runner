e6fd4d09e3248b383c2ae2be8c3f67167461f021
disaviola <37692445+disaviola@users.noreply.github.com>
2019-10-04 18:07:46 +0200

    Player stops when button is released

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -21,13 +21,14 @@ public class PlayerMovement : MonoBehaviour
     }
     private void SetMovement()
     {
-        
+
         transform.Translate(0f, speed * Time.deltaTime, 0f);
         input = new Vector3(Input.GetAxis("Horizontal"), 0.0f, 0.0f);
-        //if (input.magnitude<1 )
-        //{
-        //    input = Vector3.zero;
-        //}
+        if (!Input.anyKey)
+        {
+            input = Vector3.zero;
+        }
+
         input = input.normalized;
         transform.position += input * Time.deltaTime * 8;
         inputPreviousFrame = input;
