f6bb16ad22a79aeaa3659eee365c71a1c7570485
TeoC00l <37851342+TeoC00l@users.noreply.github.com>
2019-10-04 19:15:20 +0200

    dsfdsfds

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    [SerializeField] GameObject player;
+    private float speed = 8f;
+    private Vector3 movement;
+
+    // Start is called before the first frame update
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        addMovement();
+
+    }
+
+    private void addMovement()
+    {
+        Vector3 direction = (player.transform.position - gameObject.transform.position).normalized;
+        gameObject.transform.position += direction * speed * Time.deltaTime;
+    }
+
+
+
+}
