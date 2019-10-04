997a2406938b3d6abca3a72d01730a4280b8a7f0
paschalisTol <37561700+paschalistol@users.noreply.github.com>
2019-10-04 17:29:36 +0200

    movement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Vector3 input, inputPreviousFrame;
+    [SerializeField] private float speed = 5f;
+
+    void Update()
+    {
+        SetMovement();
+    }
+    private void SetMovement()
+    {
+        
+        transform.Translate(0f, speed * Time.deltaTime, 0f);
+        input = new Vector3(Input.GetAxis("Horizontal"), 0.0f, 0.0f);
+        //if (input.magnitude<1 )
+        //{
+        //    input = Vector3.zero;
+        //}
+        input = input.normalized;
+        transform.position += input * Time.deltaTime * 8;
+        inputPreviousFrame = input;
+    }
+}
