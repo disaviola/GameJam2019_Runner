721fb00e13161cce257728eb52391da3da408e0f
Bazzan <sebake01@gmail.com>
2019-10-05 14:45:05 +0200

    sfx

--- Assets/Scripts/Statemachine/Player.cs
+++ Assets/Scripts/Statemachine/Player.cs
@@ -5,10 +5,23 @@ using UnityEngine;
 public class Player : StateMachine
 {
     public PlayerMovement movementHandler;
+    protected AudioSource AudioSource;
+    protected AudioClip audioClip;
+    private AudioClip boink;
+    private AudioClip suck;
+
 
     protected override void Awake()
     {
         movementHandler = GetComponent<PlayerMovement>();
         base.Awake();
     }
+
+
+    protected void PlayBoink()
+    {
+        AudioSource.clip = boink;
+    }
+
+
 }
