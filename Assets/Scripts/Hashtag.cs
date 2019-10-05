d38c1d943caa7d2801caf73e179137fce7a74610
paschalisTol <37561700+paschalistol@users.noreply.github.com>
2019-10-05 14:31:08 +0200

    hashtag obstacle

---
+++ Assets/Scripts/Hashtag.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hashtag : MonoBehaviour
+{
+    [SerializeField] private GameObject hashtag;
+    private GameObject instantiated;
+    private float randomX, randomY;
+    void Start()
+    {
+        randomX = Random.Range(0, 3);
+        randomY = Random.Range(0, 3);
+        instantiated = Instantiate<GameObject>(hashtag, new Vector2(randomX + transform.position.x, randomY + transform.position.y), Quaternion.identity,gameObject.transform);
+    }
+
+}
