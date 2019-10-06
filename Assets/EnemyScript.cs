b1dd018a06820ca9c2b0d5a00312b9693aeee48a
TeoC00l <37851342+TeoC00l@users.noreply.github.com>
2019-10-06 13:06:32 +0200

    enemy block added

---
+++ Assets/EnemyScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyScript : MonoBehaviour
+{
+    [SerializeField] private GameObject player;
+    [SerializeField] private GameObject wall;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 enemyPosition = new Vector2(player.transform.position.x, wall.transform.position.y);
+        gameObject.transform.position = enemyPosition;
+    }
+}
